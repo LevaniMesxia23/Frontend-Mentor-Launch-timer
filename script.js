96f71ad84ac8c00fc730cc5b3f6663e05824c04e
levan mesxia <levanmesxia@levans-MacBook-Pro.local>
2024-02-19 23:10:13 +0400

    updated project

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-const year = document.querySelector(".days")
+const days = document.querySelector(".days")
 const hours = document.querySelector(".hours")
 const minutes = document.querySelector(".minutes")
 const seconds = document.querySelector(".seconds")
 
-year.innerHTML = 13
+days.innerHTML = 13
 hours.innerHTML = 23
-minutes.innerHTML = 60
+minutes.innerHTML = 59
 seconds.innerHTML = 60
 
-
-let minutesDown = minutes.innerHTML -= 1
 function comeDownTime(){
   let secondsDown = seconds.innerHTML -= 1
-  if(secondsDown == 0){
-    minutes.innerHTML -= 1
+  if(secondsDown < 0){
+    if(minutes.innerHTML != 0){
+      minutes.innerHTML -= 1
+    }else{
+      minutes.innerHTML -= 0
+    }
     seconds.innerHTML = 59
   }
 
-
-  if(minutes.innerHTML == 0){
-    hours.innerHTML -= 1
-    minutes.innerHTML = 59
+  if(minutes.innerHTML == 0 && seconds.innerHTML == 0){
+    if(hours.innerHTML != 0){
+      minutes.innerHTML = 60
+      hours.innerHTML -= 1
+    } else{
+      hours.innerHTML -= 0
+    }
   }
-
+  
   if(hours.innerHTML == 0 && minutes.innerHTML == 0 && seconds.innerHTML == 0){
-    year.innerHTML -= 1
-    hours.innerHTML = 23
+    if(days.innerHTML != 0){
+      hours.innerHTML = 24
+      days.innerHTML -= 1
+    } else{
+      days.innerHTML -= 0
+    }
   } 
+
+  if(days.innerHTML == 0 && hours.innerHTML == 0 && minutes.innerHTML == 0 && seconds.innerHTML == 0){
+    days.innerHTML = 0
+    hours.innerHTML = 0
+    minutes.innerHTML = 0
+    seconds.innerHTML = 0
+  }
 }
 setInterval(comeDownTime, 1000)
